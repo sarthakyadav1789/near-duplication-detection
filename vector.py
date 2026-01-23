f29fa4e93cf8efee3e832473d1589c90b5e3bf1c
rian-core <anandricha710@gmail.com>
2026-01-23 19:07:24 +0530

    vector.py

--- vector.py
+++ vector.py
@@ -3,13 +3,13 @@ import open_clip
 from PIL import Image
 import os
 
-# ================= CONFIG =================
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 MODEL_PATH = "E:/models/open_clip_model.safetensors"
 MODEL_NAME = "ViT-B-32"
 
-# ================= LOAD MODEL (ONCE) =================
+# Loading Model
 model, _, preprocess = open_clip.create_model_and_transforms(
     MODEL_NAME,
     pretrained=MODEL_PATH
@@ -17,18 +17,18 @@ model, _, preprocess = open_clip.create_model_and_transforms(
 model = model.to(DEVICE)
 model.eval()
 
-
+#embeddings
 def get_image_embedding(image_path):
     if not os.path.exists(image_path):
         raise FileNotFoundError(f"Image not found: {image_path}")
 
-    # Load & preprocess image
+    # Loading & preprocessing image
     img = Image.open(image_path).convert("RGB")
-    img_tensor = preprocess(img).unsqueeze(0).to(DEVICE)  # (1, C, H, W)
+    img_tensor = preprocess(img).unsqueeze(0).to(DEVICE)  
 
-    # Encode
+    # Encoding and normalization
     with torch.no_grad():
         embedding = model.encode_image(img_tensor)
         embedding /= embedding.norm(dim=-1, keepdim=True)
 
-    return embedding.squeeze(0).cpu()  # shape: (dim,)
+    return embedding.squeeze(0).cpu() 
